ef23e87beebf76dd73efefafb83c449cb6052ac8
Abir Mohammad Hadi <abir@nerddevs.com>
2021-04-22 23:08:50 +0600

    FIx

--- src/components/CountryDetails/CountryDetails.js
+++ src/components/CountryDetails/CountryDetails.js
@@ -9,8 +9,17 @@ const CountryDetails = () => {
         fetch(url)
         .then(res => res.json())
         .then(data => setDetail(data))
-    }, [])
-    const { name,capital } = detail[0];
+    }, []);
+
+    // It takes some time to load the country detaul from useEffects
+    // That's called Asynchronous task
+    // When it gets loaded then we can show the name
+    // So we check whether the `detail` array has any element in it
+    // so detail.length gives >= 0, 0 means no data is added so we can't see any
+    // Whenerver the data is loaded in details, means the HTTP request in useEffect completes,
+    // This condition becomes true, and data gets rendered.
+    // This is called Reactivity, hence we have ReactJS
+    const { name,capital } = detail.length && detail[0];
     return (
         <div>
             <h1>This is country details</h1>
